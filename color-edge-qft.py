a59d969a0b5d8b1659a2d7531d87f11ec1b46138
jclarriaga <carlos_gara_1507@hotmail.com>
2022-12-06 15:43:43 -0600

    Update Code

--- color-edge-qft.py
+++ color-edge-qft.py
@@ -76,7 +76,7 @@ def img_iqft(img_qft, mu):
     fmn : Color image.
     """
 
-    assert F.shape[2] == 4, "Image is not in qft format"
+    assert img_qft.shape[2] == 4, "Image is not in qft format"
 
     A = img_qft[:, :, 0]
     B = img_qft[:, :, 1]
